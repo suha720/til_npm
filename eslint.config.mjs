9eaf9d7ae8f6347f3a83c310b1e0c2beee5881d0
suha720 <a50914513@gmail.com>
2025-07-21 12:35:26 +0900

    [docs] 프로젝트 기본 세팅

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-plugin-prettier";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      react: pluginReact,
+      prettier: pluginPrettier,
+    },
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+      "prettier/prettier": "warn",
+      "no-var": "warn", // ✅ 선언했지만 사용하지 않으면 에러
+      "no-unused-vars": "warn",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+]);
