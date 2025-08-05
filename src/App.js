d32dceab934e99d111c6b9fb8203bb3e8b087e1c
suha720 <a50914513@gmail.com>
2025-08-05 13:05:49 +0900

    [docs] react icons의 기초이해

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import { useEffect } from "react";
-import { getTodos } from "./apis/todoApi";
-
+import { FaSearch } from "react-icons/fa";
+import { FaStar } from "react-icons/fa6";
 function App() {
-  // js 자리
-
-  useEffect(() => {
-    getTodos();
-  }, []);
-  // jsx 자리
-  return <div>App</div>;
+  const point = 10;
+  const rate = 3;
+  return (
+    <div>
+      <h1>
+        App <FaSearch />
+      </h1>
+      <div>
+        <h2>
+          당신의 별점(
+          <FaStar style={{ color: "orangered", fontSize: 20 }} />
+          )은?{" "}
+        </h2>
+        <div>
+          {[...Array(point)].map((item, index) => {
+            return (
+              <FaStar
+                key={index}
+                style={{
+                  color: index < rate ? "gold" : "gray",
+                }}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
